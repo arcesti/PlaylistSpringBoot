0a70f1466aa94636a0d147b4858e586db28384a1
arcesti <arcesti15@gmail.com>
2025-04-04 05:47:41 -0300

    Finalizado

--- src/main/java/unoeste/fipp/playmysongsbackend/restcontrollers/MusicRestController.java
+++ src/main/java/unoeste/fipp/playmysongsbackend/restcontrollers/MusicRestController.java
@@ -2,7 +2,6 @@ package unoeste.fipp.playmysongsbackend.restcontrollers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,12 +12,14 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "apis")
 public class MusicRestController {
     @Autowired
     private HttpServletRequest request;
     private static final String UPLOAD_FOLDER = "src/main/resources/static/uploads/";
+    private String nomeArq;
     @PostMapping(value = "add-music")
     public ResponseEntity<Object> addUsuario(@RequestParam("file") MultipartFile file,
                                              @RequestParam("titulo") String titulo,
@@ -28,9 +29,19 @@ public class MusicRestController {
         try {
             //cria uma pasta na área estática para acomodar os arquivos recebidos, caso não exista
             File uploadFolder = new File(UPLOAD_FOLDER);
+            System.out.println(uploadFolder.getAbsolutePath());
             if (!uploadFolder.exists()) uploadFolder.mkdir();
-            // criar um nome para o arquivo
-            file.transferTo(new File(uploadFolder.getAbsolutePath() + "/"+file.getOriginalFilename()));
+            // Gerar um nome personalizado para o arquivo
+            String extensao = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            titulo = titulo.replaceAll("_", "").replaceAll("\\s+", "");
+            artista = artista.replaceAll("_", "").replaceAll("\\s+", "");
+            estilo = estilo.replaceAll("_", "").replaceAll("\\s+", "");
+            String novoNomeArquivo = titulo + "_" + artista + "_" + estilo + extensao;
+            nomeArq = novoNomeArquivo;
+            System.out.println(novoNomeArquivo);
+            // Substituir espaços e caracteres indesejados para segurança
+            // Substituir espaços e caracteres indesejados para segurança
+            file.transferTo(new File(uploadFolder.getAbsolutePath() + "/" + novoNomeArquivo));
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new Erro("Erro ao armazenar o arquivo. " + e.getMessage()));
         }
@@ -44,7 +55,31 @@ public class MusicRestController {
         // CASO LISTA VAZIA, RETORNE ERRO
         List<Musica> musicaList = new ArrayList<>();
         for (String f : files) {
-            musicaList.add(new Musica("titulo", "artista", "estilo", getHostStatic()+f));
+            StringBuilder titulo = new StringBuilder();
+            StringBuilder artista = new StringBuilder();
+            StringBuilder estilo = new StringBuilder();
+            int cont = 0;
+
+            for (int i = 0; i < f.length(); i++) {
+                char c = f.charAt(i);
+
+                if (cont == 0 && c != '_') {
+                    titulo.append(c);  // Adiciona ao título
+                } else if (cont == 1 && c != '_') {
+                    artista.append(c);  // Adiciona ao artista
+                } else if (cont == 2 && c != '_') {
+                    estilo.append(c);  // Adiciona ao estilo
+                }
+
+                // Se encontrar um '_', muda para o próximo campo
+                if (c == '_') {
+                    cont++;
+                }
+            }
+
+            // Criação da nova instância de Musica
+            musicaList.add(new Musica(titulo.toString(), artista.toString(), estilo.toString(), getHostStatic() + f));
+
         }
         return ResponseEntity.ok(musicaList);
     }
